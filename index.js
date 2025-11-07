533bd47823b5ae138488cc447182b35a647c2ca3
Ellaine Gonzales <ellainegonzales003@gmail.com>
2025-11-07 17:42:03 +0800

    Added Static css files

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs")
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
